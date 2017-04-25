19675b30fd3a7e8101f592befcacb960f4c0bcb2
Victor Carballeda <victorcarba@gmail.com>
2017-04-25 23:59:32 +0200

    Updates

--- js/scroll.js
+++ js/scroll.js
@@ -1,63 +1,39 @@
-/*
-Smooth scrolling tablet
-*/
-  /*  $(document).ready(function() {
-
-$("#pasaaño-1").addClass("active");
-
-$("#pasaaño-1").click(function() {
-     $('.scroll').animate({
-         scrollTop:        $("#año-1").offset().top-20
-     }, 5000);
-  return false;
- });
-
-$("#pasaaño-2").click(function() {
-     $('.scroll').animate({
-         scrollTop:        $("#año-2").offset().top-20
-     }, 1000);
-  return false;
- });
-
-$("#pasaaño-3").click(function() {
-     $('.scroll').animate({
-         scrollTop:        $("#año-3").offset().top-20
-     }, 1000);
-  return false;
- });
-
- $("#pasaaño-4").click(function() {
-   $(this).addClass("active");
-      $('.scroll').animate({
-          scrollTop:        $("#año-4").offset().top-20
-      }, 1000);
-   return false;
-  });
-
-  $("#pasaaño-5").click(function() {
-    $(this).addClass("active");
-       $('.scroll').animate({
-           scrollTop:        $("#año-5").offset().top-20
-       }, 1000);
-    return false;
-   });
-      });
+$(document).ready(function() {
 
 
-   $("#pasaaño-6").click(function() {
-     $(this).addClass("active");
-        $('.scroll').animate({
-            scrollTop:        $("#año-6").offset().top-20
-        }, 1000);
-     return false;
+    $("#pasaaño-1").click(function() {
+        $('.scroll').scrollTo($('#año-1'), 1000);
+        return false;
     });
 
+    $("#pasaaño-2").click(function() {
+        $('.scroll').scrollTo($('#año-2'), 1000);
+        return false;
+    });
+
+    $("#pasaaño-3").click(function() {
+        $('.scroll').scrollTo($('#año-3'), 1000);
+        return false;
+    });
+
+    $("#pasaaño-4").click(function() {
+        $('.scroll').scrollTo($('#año-4'), 1000);
+        return false;
+    });
+
+    $("#pasaaño-5").click(function() {
+        $('.scroll').scrollTo($('#año-5'), 1000);
+        return false;
+    });
+
+    $("#pasaaño-6").click(function() {
+        $('.scroll').scrollTo($('#año-6'), 1000);
+        return false;
+    });
 
     $("#pasaaño-7").click(function() {
-      $(this).addClass("active");
-         $('.scroll').animate({
-             scrollTop:        $("#año-7").offset().top-20
-         }, 1000);
-      return false;
-     });
-*/
+        $('.scroll').scrollTo($('#año-7'), 1000);
+        return false;
+    });
+
+});
